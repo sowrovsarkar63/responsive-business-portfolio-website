1450bb19b1b7041c8bceb4dc54d71b198dcfff8d
sowrovsarkar63 <dev.sowrovsarkar63@gmail.com>
2021-06-16 13:47:01 +0600

    Hero section and navbar done

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Glightbox plugins
+const lightbox = GLightbox({
+  touchNavigation: true,
+  href: "https://www.youtube.com/watch?v=iJKCj8uAHz8",
+  type: "video",
+  source: "youtube", //vimeo, youtube or local
+  width: 900,
+});
